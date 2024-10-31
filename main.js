161c7f3944c612f9ba1cb6350084aac7bae6f35b
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2024-10-31 14:01:58 -0400

    create more testing

--- main.js
+++ main.js
@@ -18,3 +18,22 @@ test.set('lion', 'golden');
 console.log(test.remove('apple'));
 console.log(test.has('apple'));
 console.log(test.length());
+console.log(test.capacity);
+console.log(test.entries());
+
+test.set('apple', 'red');
+test.set('grape', 'wonka');
+test.set('moon', 'silver');
+
+console.log(test.length());
+console.log(test.capacity);
+console.log(test.entries());
+console.log(test.get('grape'));
+console.log(test.has('kite'));
+console.log(test.keys());
+console.log(test.values());
+
+test.clear();
+console.log(test.length());
+console.log(test.capacity);
+console.log(test.entries());
